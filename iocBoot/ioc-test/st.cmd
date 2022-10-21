66fd660b6c018b44afd5043d39e8f101e67918d0
Ken Lauer <klauer@slac.stanford.edu>
2022-10-21 11:10:30 -0700

    Initial commit

---
+++ iocBoot/ioc-test/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/rhel7-x86_64/test
+
+#- You may have to change test to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/test.dbd"
+test_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("missing_dbfile.db","user=klauer")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=klauer"
