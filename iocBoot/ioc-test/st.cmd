cbcf1527a318be5b0921568379d95d05afe34145
Ken Lauer <klauer@slac.stanford.edu>
2023-09-27 12:53:00 -0700

    MNT: simplify startup script directories

--- iocBoot/ioc-test/st.cmd
+++ iocBoot/ioc-test/st.cmd
@@ -1,24 +1,21 @@
 #!../../bin/rhel7-x86_64/newtest
-# arbitrary change 1
-
 #- You may have to change test to something else
 #- everywhere it appears in this file
 
 < envPaths
 
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/test.dbd"
+dbLoadDatabase "../../dbd/test.dbd"
 test_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 dbLoadRecords("missing_dbfile.db","user=klauer")
-
-cd "${TOP}/iocBoot/${IOC}"
 dbLoadRecords("test.db","user=klauer")
 
 iocInit
+
+cd $(TOP)
+dbl
 
 ## Start any sequence programs
 #seq sncxxx,"user=klauer"
