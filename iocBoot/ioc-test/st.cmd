e44b19c2f53705f79dd19eb7485c7a314cc3b441
Ken Lauer <klauer@slac.stanford.edu>
2023-09-28 11:14:43 -0700

    WIP: more things to show in docs

--- iocBoot/ioc-test/st.cmd
+++ iocBoot/ioc-test/st.cmd
@@ -4,13 +4,25 @@
 
 < envPaths
 
+# Some configuration for this example:
+epicsEnvSet "STREAM_PROTOCOL_PATH", "."
+
 ## Register all support components
 dbLoadDatabase "../../dbd/test.dbd"
 test_registerRecordDeviceDriver pdbbase
 
+asSetFilename("example.acf")
+
 ## Load record instances
 dbLoadRecords("missing_dbfile.db","user=klauer")
-dbLoadRecords("test.db","user=klauer")
+dbLoadRecords("test.db","P=IOC:TEST:,user=klauer")
+
+# Access control example:
+dbLoadRecords("acf.db","P=IOC:ACF:,user=klauer")
+
+# StreamDevice example:
+drvAsynIPPortConfigure("terminal", "localhost:40000")
+dbLoadRecords("stream.db","P=IOC:STREAM:")
 
 iocInit
 
